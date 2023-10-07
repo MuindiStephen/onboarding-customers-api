355b40c760703bb5d0f023715a08a9ce5501cd62
MuindiStephen <stephenmuindi241@gmail.com>
2023-10-07 19:10:13 +0300

    entity: User

---
+++ src/main/java/com/stevemd/onboarding/model/User.java
@@ -0,0 +1,31 @@
+
+package com.stevemd.onboarding.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "user_sequence")
+    @SequenceGenerator(name = "user_sequence",sequenceName = "user_sequence",allocationSize = 1)
+    private Long id;
+
+    @Column(name = "email",nullable = false)
+    private String name;
+
+    @Column(name = "name",nullable = false)
+    private String email;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+}
